bf6af8e107d9586d02e02dd3ac62730b8874cb10
ikoskin <64212607+ikoskinn@users.noreply.github.com>
2023-01-30 14:49:57 +0000

    v1.4
    - Now script will create a tables with phone number in name of table.

--- Postgre/postgreManager.py
+++ Postgre/postgreManager.py
@@ -13,7 +13,7 @@ class Postgre:
     connection = None
     cursor = None
 
-    def __init__(self, user, password, host, port, database):
+    def __init__(self, user, password, host, port, database, phone_number):
         try:
             # Connect to an existing database
             self.connection = psycopg2.connect(user=user,
@@ -21,8 +21,11 @@ class Postgre:
                                                host=host,
                                                port=port,
                                                database=database)
+            self.Phone = phone_number
 
             self.ChannelsReserved = {}
+            self.ChannelsTableName = f'channels_{self.Phone}'
+            self.AdminsTableName = f'admins_{self.Phone}'
 
             # Create a cursor to perform database operations
             self.cursor = self.connection.cursor()
@@ -34,25 +37,25 @@ class Postgre:
             # Fetch result
             record = self.cursor.fetchone()
             print("You are connected to - ", record, "\n")
-            if not self.fetchOne('''SELECT EXISTS (
+            if not self.fetchOne(f'''SELECT EXISTS (
                            SELECT FROM information_schema.tables 
                            WHERE  table_schema = 'public'
-                           AND    table_name   = 'admins'
+                           AND    table_name   = 'admins_{self.Phone}'
                            );''')[0]:
-                print("Создаем таблицу Admins")
+                print(f"Создаем таблицу admins_{self.Phone}")
                 self.execute(
-                    '''CREATE TABLE Admins(
+                    f'''CREATE TABLE admins_{self.Phone}(
                     USER_ID           BIGINT    NOT NULL,
                     USERNAME           TEXT); ''')
 
-            if not self.fetchOne('''SELECT EXISTS (
+            if not self.fetchOne(f'''SELECT EXISTS (
                                        SELECT FROM information_schema.tables 
                                        WHERE  table_schema = 'public'
-                                       AND    table_name   = 'channels'
+                                       AND    table_name   = 'channels_{self.Phone}'
                                        );''')[0]:
-                print("Создаем таблицу Channels")
+                print(f"Создаем таблицу channels_{self.Phone}")
                 self.execute(
-                    '''CREATE TABLE Channels(
+                    f'''CREATE TABLE channels_{self.Phone}(
                     CHAT_ID           BIGINT    NOT NULL,
                     USERNAME           TEXT    NOT NULL,
                     LAST_POST_ID           BIGINT    NOT NULL,
@@ -65,61 +68,50 @@ class Postgre:
                 print("PostgreDB connection is closed")
 
     def ChannelExists(self, Channel: telethon.tl.types.Channel) -> bool:
-        result = self.fetchOne(f"SELECT * FROM Channels WHERE CHAT_ID = {Channel.id}")
+        result = self.fetchOne(f"SELECT * FROM {self.ChannelsTableName} WHERE CHAT_ID = {Channel.id}")
         if result is not None:
             return True
         else:
             return False
 
     def AddChannel(self, Channel: telethon.tl.types.Channel, LastPostId: int):
-        self.execute(f'INSERT INTO Channels(CHAT_ID, USERNAME, LAST_POST_ID) VALUES(\'{Channel.id}\', \'{Channel.username}\', \'{LastPostId}\')')
+        self.execute(f'INSERT INTO {self.ChannelsTableName}(CHAT_ID, USERNAME, LAST_POST_ID) VALUES(\'{Channel.id}\', \'{Channel.username}\', \'{LastPostId}\')')
 
     def DeleteChannel(self, Channel: telethon.tl.types.Channel):
-        self.execute(f'DELETE FROM Channels WHERE CHAT_ID = \'{Channel.id}\'')
+        self.execute(f'DELETE FROM {self.ChannelsTableName} WHERE CHAT_ID = \'{Channel.id}\'')
 
-    def GetUnreservedChannels(self, max = 20):
-        chs = self.GetAllChannels()
-        channels = {}
-        i = 0
-        for ch in chs:
-            if ch[1] not in self.ChannelsReserved:
-                channels[ch[1]] = ch
-                self.ChannelsReserved[ch[1]] = ch
-                i = i + 1
-                if i >= max:
-                    break
+    def GetChannels(self):
+        t10 = int(time.time()) - 10
+        t = int(time.time())
+        chs = self.fetchAll(f'WITH updated as ('
+         f'UPDATE {self.ChannelsTableName} SET checked_time = {t} WHERE (checked_time <= {t10} OR checked_time = 0 OR checked_time IS NULL) RETURNING *)'
+         f' SELECT u.*, COUNT(*) OVER () AS total_update_count FROM updated AS u'
+        )
 
-        return channels
-
-    def UnreserveChannels(self, Channels: dict):
-        for ch_username in Channels:
-            del self.ChannelsReserved[ch_username]
-
-
-
-        # f'WITH updated as ('
-        # f'UPDATE Channels SET CHECKED_TIME = \'{dat}\' WHERE CHECKED_TIME <= \'{dat_fwd}\' RETURNING *)'
-        # f' SELECT u.*, COUNT(*) OVER () AS total_update_count FROM updated AS u LIMIT {max}'
+        return chs
 
     def GetAllChannels(self):
         with lock:
-            return self.fetchAll(f'SELECT * FROM Channels')
+            return self.fetchAll(f'SELECT * FROM {self.ChannelsTableName}')
 
     def UpdateLastPostId(self, Channel: telethon.tl.types.Channel, LastPostId: int):
-        self.execute(f'UPDATE Channels SET LAST_POST_ID = {LastPostId} WHERE CHAT_ID = \'{Channel.id}\'')
+        self.execute(f'UPDATE {self.ChannelsTableName} SET LAST_POST_ID = {LastPostId} WHERE CHAT_ID = \'{Channel.id}\'')
+
+    def UpdateLastPostIdByInt(self, Channel_id: int, LastPostId: int):
+        self.execute(f'UPDATE {self.ChannelsTableName} SET LAST_POST_ID = {LastPostId} WHERE CHAT_ID = \'{Channel_id}\'')
 
     def IsAdmin(self, User: telethon.tl.types.User) -> bool:
-        result = self.fetchOne(f'SELECT * FROM Admins WHERE USER_ID = \'{User.id}\'')
+        result = self.fetchOne(f'SELECT * FROM {self.AdminsTableName} WHERE USER_ID = \'{User.id}\'')
         if result is not None:
             return True
         else:
             return False
 
     def AddAdmin(self, User: telethon.tl.types.User):
-        self.execute(f'INSERT INTO Admins(USER_ID, USERNAME) VALUES(\'{User.id}\', \'{User.username}\')')
+        self.execute(f'INSERT INTO {self.AdminsTableName}(USER_ID, USERNAME) VALUES(\'{User.id}\', \'{User.username}\')')
 
     def DeleteAdmin(self, User: telethon.tl.types.User):
-        self.execute(f'DELETE FROM Admins WHERE USER_ID = \'{User.id}\'')
+        self.execute(f'DELETE FROM {self.AdminsTableName} WHERE USER_ID = \'{User.id}\'')
 
     def execute(self, query):
         try:
